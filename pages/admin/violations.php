59095682ba23d8436443f718cc398bca9e839d4e
ZenithSUS <jeranmerino147@gmail.com>
2025-04-08 13:14:58 +0800

    Remove the footers and ignore the message sending if no employee is being evaluated

--- pages/admin/violations.php
+++ pages/admin/violations.php
@@ -312,7 +312,7 @@
                 <!-- delete modal end -->
             </div>
             <!--  BEGIN FOOTER  -->
-            <div class="footer-wrapper">
+            <!-- <div class="footer-wrapper">
                 <div class="footer-section f-section-1">
                     <p class="">Copyright © <span class="dynamic-year">2022</span> <a target="_blank" href="https://designreset.com/cork-admin/">DesignReset</a>, All rights reserved.</p>
                 </div>
@@ -321,7 +321,7 @@
                             <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                         </svg></p>
                 </div>
-            </div>
+            </div> -->
             <!--  END FOOTER  -->
         </div>
         <!--  END CONTENT AREA  -->
